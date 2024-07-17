39b22ca29a6822f868fcb4dc0da72349f57bee2c
Federico Vazzoler <vazzoler.federico@gmail.com>
2024-07-17 11:25:57 +0200

    Improve environment script to export the local conda location as a bash variable

--- environment.sh
+++ environment.sh
@@ -2,16 +2,38 @@
 
 # Function to check if conda is installed
 check_conda() {
-    if ! command -v conda &> /dev/null
-    then
+    if ! command -v conda &> /dev/null; then
         echo "conda could not be found. Please install Anaconda or Miniconda and try again."
         exit 1
+    else
+        case "$(uname)" in
+            "Darwin")
+                export CONDA_PATH="${CONDA_EXE}"
+                export CONDA_PYTHON_PATH="${CONDA_PYTHON_EXE}"
+                echo "Created env. variable CONDA_PATH=${CONDA_PATH}"
+                echo "Created env. variable CONDA_PYTHON_PATH=${CONDA_PYTHON_PATH}"
+                export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+                export DYLD_LIBRARY_PATH=$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH
+                export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
+                ;;
+            "Linux")
+                export CONDA_PATH="${CONDA_EXE}"
+                export CONDA_PYTHON_PATH="${CONDA_PYTHON_EXE}"
+                echo "Created env. variable CONDA_PATH=${CONDA_PATH}"
+                echo "Created env. variable CONDA_PYTHON_PATH=${CONDA_PYTHON_PATH}"
+                ;;
+            *)
+                echo "unsupported operating system"
+                exit 1
+                ;;
+        esac
     fi
 }
 
 # Function to activate the conda environment
 activate_environment() {
     local env_name=$1
+    conda=${CONDA_PATH}
     source "$(conda info --base)/etc/profile.d/conda.sh"
     conda activate "$env_name"
     if [ $? -ne 0 ]; then
@@ -27,7 +49,6 @@ activate_environment() {
 # Main script
 main() {
     check_conda
-    activate_environment "minnlo-env"
 
     # Determine the directory where the script is located
     SOURCE="${BASH_SOURCE[0]}"
@@ -43,6 +64,8 @@ main() {
     # Export the pdf sets location
     export LHAPDF_DATA_PATH="/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/"
     echo "Created env. variable LHAPDF_DATA_PATH=${LHAPDF_DATA_PATH}"
+    
+    activate_environment "minnlo-env"
 }
 
 main
